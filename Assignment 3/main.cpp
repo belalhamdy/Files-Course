3b8095130660d31356508db02ba50e0b1297cdf5
Megadardery <Megadardery@yahoo.com>
2019-04-11 01:21:27 +0200

    finished the disaster that is this assignment

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -2,27 +2,30 @@
 using namespace std;
 
 const char DELIM = '|';
+#define EMPTYID { ' ',' ',' ',' ',' ',' ' }
+fstream dataFile;
 
-fstream filedata;
+//fixed-length fields (int nxt, char ID[6])
+fstream invertedList;
 
 struct Course {
-	char ID[6];
+	char ID[6] = EMPTYID;
 	string name;
 	string instructor;
 	short weeks;
 };
 
 struct Primary {
-	char ID[6];
+	char ID[6] = EMPTYID;
 	int offset;
-	bool operator<(const Primary & b) {
+	bool operator<(const Primary & b) const {
 		int cmp = strcmp(ID, b.ID);
 		if (cmp != 0)
 			return cmp < 0;
 		else
 			return offset < b.offset;
 	}
-	bool operator==(const Primary & b) {
+	bool operator==(const Primary & b) const {
 		return strcmp(ID, b.ID) == 0;
 	}
 };
@@ -30,20 +33,22 @@ struct Primary {
 struct Secondry {
 	string key;
 	int labelID;
-	bool operator<(const Secondry & b) {
+	bool operator<(const Secondry & b) const {
 		return key < b.key;
 	}
-	bool operator==(const Secondry & b) {
+	bool operator==(const Secondry & b) const {
 		return key == b.key;
 	}
- };
+};
+
+const int INVERTED_SIZE = sizeof(-1) + sizeof Course::ID;
 
 vector<Primary> primaryIdx;
 vector<Secondry> secondryIdx;
 
 template<typename T>
 int binarySearch(vector<T> vec, T val) {
-	
+
 	int sz = vec.size();
 	int st = 0, en = sz - 1;
 	while (st <= en)
@@ -61,61 +66,121 @@ int binarySearch(vector<T> vec, T val) {
 
 
 void openFile(fstream& file, const char* filename) {
-	file.open(filename, ios::in | ios::out);
+	file.open(filename, ios::in | ios::out | ios::binary);
 	if (!file.is_open()) {
-		file.open(filename, ios::out);
+		file.open(filename, ios::out | ios::binary);
 		file.close();
-		file.open(filename, ios::in | ios::out);
+		file.open(filename, ios::in | ios::out | ios::binary);
 		if (!file.is_open()) exit(0);
 	}
 }
-
 void writeCourse(const Course& c) {
 	short len = sizeof c.ID - 1 + c.name.length() + c.instructor.length() + sizeof c.weeks + 4;
 
-	filedata.write((char*)&len, sizeof len);
-	filedata.write(c.ID, sizeof c.ID - 1);
-	filedata.put('|');
-	filedata.write(c.name.c_str(), c.name.length());
-	filedata.put('|');
-	filedata.write(c.instructor.c_str(), c.instructor.length());
-	filedata.put('|');
-	filedata.write((char*)&c.weeks, sizeof c.weeks);
-	filedata.put('|');
+	dataFile.write((char*)&len, sizeof len);
+	dataFile.write(c.ID, sizeof c.ID - 1);
+	dataFile.put('|');
+	dataFile.write(c.name.c_str(), c.name.length());
+	dataFile.put('|');
+	dataFile.write(c.instructor.c_str(), c.instructor.length());
+	dataFile.put('|');
+	dataFile.write((char*)&c.weeks, sizeof c.weeks);
+	dataFile.put('|');
 
+}
+void addCourse(const Course& c) {
+	dataFile.seekp(0, ios::end);
+
+	//primary index
+	Primary idx;
+	strcpy(idx.ID, c.ID);
+	idx.offset = dataFile.tellp();
+	primaryIdx.insert(lower_bound(primaryIdx.begin(), primaryIdx.end(), idx), idx);
+
+	//secondry index
+	Secondry idxS;
+	idxS.key = c.instructor;
+	auto it = lower_bound(secondryIdx.begin(), secondryIdx.end(), idxS);
+	int lst;
+
+	if (it == secondryIdx.end() || it->key != idxS.key)
+		lst = -1;
+	else
+		lst = it->labelID;
+
+	invertedList.seekp(0, ios::end);
+	idxS.labelID = invertedList.tellp() / INVERTED_SIZE;
+	invertedList.write((char*)&lst, sizeof lst);
+	invertedList.write(c.ID, sizeof c.ID);
+
+	if (lst == -1)
+		secondryIdx.insert(it, idxS);
+	else
+		it->labelID = idxS.labelID;
+	writeCourse(c);
 }
 void readCourse(Course& c) {
 	short len;
 
-	filedata.read((char*)&len, sizeof len);
-	filedata.getline(c.ID, sizeof c.ID, '|');
-	getline(filedata, c.name, '|');
-	getline(filedata, c.instructor, '|');
-	filedata.read((char*)&c.weeks, sizeof c.weeks);
-	filedata.get();
+	dataFile.read((char*)&len, sizeof len);
+	dataFile.getline(c.ID, sizeof c.ID, '|');
+	getline(dataFile, c.name, '|');
+	getline(dataFile, c.instructor, '|');
+	dataFile.read((char*)&c.weeks, sizeof c.weeks);
+	dataFile.get();
 
 }
-int updateCourse(const Course& c) {
-	short oldlen, newlen;
-	newlen = sizeof c.ID - 1 + c.name.length() + c.instructor.length() + sizeof c.weeks + 4;
-	filedata.read((char*)&oldlen, sizeof oldlen);
-	
-	filedata.seekp((int)filedata.tellg() - sizeof oldlen);
+void deleteSingleCourse(bool deleteSecondry) {
+	Course c;
+	int off = dataFile.tellg();
+	readCourse(c);
 
-	int offset = filedata.tellp();
-	if (oldlen < newlen) {
-		filedata.put('*');
-		filedata.seekp(0, ios::end);
-		offset = filedata.tellp();
-		writeCourse(c);
+	//seek to the begining of the ID.
+	dataFile.seekp(off + sizeof short(-1));
+	dataFile.put('*');
+
+	Primary tmp;
+	strcpy(tmp.ID, c.ID);
+	int primI = binarySearch(primaryIdx, tmp);
+	primaryIdx.erase(primaryIdx.begin() + primI);
+	if (deleteSecondry) {
+		Secondry tmp2;
+		tmp2.key = c.instructor;
+		int secI = binarySearch(secondryIdx, tmp2);
+
+		int nxt = secondryIdx[secI].labelID;
+		int prev = -1;
+		int cur;
+		while (nxt != -1) {
+			cur = nxt;
+			char id[6];
+			invertedList.seekg(cur * INVERTED_SIZE);
+			invertedList.read((char*)&nxt, sizeof nxt);
+			invertedList.read(id, sizeof id);
+			if (strcmp(id, c.ID) == 0){
+				if (prev == -1)
+					secondryIdx[secI].labelID = nxt;
+				else {
+					invertedList.seekp(prev * INVERTED_SIZE);
+					invertedList.write((char*)&nxt, sizeof nxt);
+				}
+				break;
+			} 
+			prev = cur;
+		}
+		if (secondryIdx[secI].labelID == -1)
+			secondryIdx.erase(secondryIdx.begin() + secI);
 	}
-	else {
-		writeCourse(c);
-	}
-	return offset;
+}
+
+void updateCourse(const Course& c) {
+	deleteSingleCourse(true);
+	dataFile.seekp(0,ios::end);
+	addCourse(c);
 }
 
 void coutCourse(const Course& c) {
+	cout << "\n";
 	cout << "Course ID  : " << c.ID << "\n";
 	cout << "Course Name: " << c.name << "\n";
 	cout << "Instructor : " << c.instructor << "\n";
@@ -133,32 +198,36 @@ void cinCourse(Course& c) {
 	cin >> c.weeks;
 }
 
-
 bool getStatus(const char* filename) {
-	ifstream file(filename);
+	ifstream file(filename, ios::binary);
+	if (!file.is_open()) return 0;
 	char value = file.get();
-	return value == 0 ? 1 : 0;
 	file.close();
+	return value == 0 ? 1 : 0;
 }
 void setStatus(const char* filename, bool uptodate) {
 	char value = uptodate ? 0 : -1;
-	ofstream file(filename);
+	ofstream file(filename, ios::binary);
 	file.seekp(0);
 	file.put(value);
 	file.close();
 }
 
+#pragma region PrimarySecondry
+
 
 //fixed-length fields and fixed-length records
 void reconstructPrimary() {
 	int offset = 0;
-	filedata.seekg(offset);
+	dataFile.seekg(offset);
 	primaryIdx.clear();
-	while (filedata.peek() != EOF) {
-		offset = filedata.tellg();
+	while (dataFile.peek() != EOF) {
+		offset = dataFile.tellg();
 
 		Course course;
 		readCourse(course);
+
+		if (course.ID[0] == '*') continue;
 
 		Primary idx;
 		strcpy(idx.ID, course.ID);
@@ -166,7 +235,7 @@ void reconstructPrimary() {
 
 		primaryIdx.push_back(idx);
 	}
-	filedata.clear();
+	dataFile.clear();
 	sort(primaryIdx.begin(), primaryIdx.end());
 }
 void fetchPrimary() {
@@ -176,20 +245,20 @@ void fetchPrimary() {
 		return;
 	}
 	primaryIdx.clear();
-	ifstream prim("primary.txt");
+	ifstream prim("primary.txt", ios::binary);
 	prim.seekg(1);
 	while (prim.peek() != EOF) {
 		Primary cur;
 		prim.read(cur.ID, sizeof cur.ID);
 		prim.read((char*)&cur.offset, sizeof cur.offset);
-			
+
 		primaryIdx.push_back(cur);
 	}
 
 	prim.close();
 }
 void flushPrimary() {
-	ofstream prim("primary.txt");
+	ofstream prim("primary.txt", ios::trunc | ios::binary);
 	char status = 0;
 	prim.put(status);
 	for (Primary cur : primaryIdx) {
@@ -201,23 +270,56 @@ void flushPrimary() {
 
 //fixed-length and fixed-length records
 void reconstructSecondry() {
-	int offset = 0;
-	filedata.seekg(offset);
-	secondryIdx.clear();
-	while (filedata.peek() != EOF) {
-		offset = filedata.tellg();
+
+	//we will load the normal secondry key from the data file
+	struct form {
+		string key;
+		char ID[6] = EMPTYID;
+		bool operator < (const form& b) const {
+			return key < b.key;
+		}
+	};
+	vector<form> secon;
+
+	dataFile.seekg(0);
+	while (dataFile.peek() != EOF) {
 
 		Course course;
 		readCourse(course);
 
-		Secondry idx;
+		if (course.ID[0] == '*') continue;
+
+		form idx;
 		idx.key = course.instructor;
 		strcpy(idx.ID, course.ID);
 
-		secondryIdx.push_back(idx);
+		secon.push_back(idx);
 	}
-	filedata.clear();
-	sort(secondryIdx.begin(), secondryIdx.end());
+	dataFile.clear();
+
+	sort(secon.begin(), secon.end());
+	//we will construct the inverted list and the required secondry key index by
+	//going over the groups of identical names
+	invertedList.seekp(0);
+
+	secondryIdx.clear();
+
+	int i = 0;
+	int lst;
+	string last;
+	int n = secon.size();
+	while (i < n) {
+		lst = -1;
+		last = secon[i].key;
+
+		while (i < n && secon[i].key == last) {
+			invertedList.write((char*)&lst, sizeof lst);
+			invertedList.write(secon[i].ID, sizeof secon[i].ID);
+			lst = i++;
+		}
+		secondryIdx.push_back({ last,lst });
+	}
+
 }
 void fetchSecondry() {
 	bool uptodate = getStatus("secondry.txt");
@@ -226,7 +328,7 @@ void fetchSecondry() {
 		return;
 	}
 	secondryIdx.clear();
-	ifstream sec("secondry.txt");
+	ifstream sec("secondry.txt", ios::binary);
 	sec.seekg(1);
 	while (sec.peek() != EOF) {
 		Secondry cur;
@@ -238,7 +340,7 @@ void fetchSecondry() {
 	sec.close();
 }
 void flushSecondry() {
-	ofstream sec("secondry.txt");
+	ofstream sec("secondry.txt", ios::trunc | ios::binary);
 	char status = 0;
 	sec.put(status);
 	for (Secondry cur : secondryIdx) {
@@ -247,6 +349,8 @@ void flushSecondry() {
 	}
 	sec.close();
 }
+
+#pragma endregion
 
 void atStartUtil();
 void addCourseUtil();
@@ -258,42 +362,78 @@ void updateCourseIDUtil();
 void updateCourseInstructorUtil();
 void atEndUtil();
 
-int maint() {
+int main() {
 	atStartUtil();
 
-	while (1) {
+	int c = -1;
+	while (c) {
 		cout <<
-			"1) Add New course\n" <<
-			"2) Delete course (by ID)\n" <<
-			"3) Delete course (by instructor)\n" <<
-			"4) Print course (by ID)\n" <<
-			"5) Print course (by instructor)\n" <<
-			"6) update course (by ID)\n" <<
-			"7) update course (by instructor)\n" <<
-			"8) Exit\n" <<
-			"Please Enter Your Choice: ";
+			"1) Add New course\n" <<
+			"2) Delete course (by ID)\n" <<
+			"3) Delete course (by instructor)\n" <<
+			"4) Print course (by ID)\n" <<
+			"5) Print courses (by instructor)\n" <<
+			"6) Update course (by ID)\n" <<
+			"7) Update first course (by instructor)\n" <<
+			"0) Exit\n" <<
 
-		int c;
+			"DEBUGGING\n"<<
+			"10) Erase all files\n" <<
+			"11) reconstruct indicies\n" <<
+			"Please Enter Your Choice: ";
+
 		cin >> c;
+		cin.ignore();
 		switch (c) {
 		case 1:
 			addCourseUtil();
+			break;
 		case 2:
 			deleteCourseIDUtil();
+			break;
 		case 3:
 			deleteCourseInstructorUtil();
+			break;
 		case 4:
 			printCourseIDUtil();
+			break;
 		case 5:
 			printCourseInstructorUtil();
+			break;
 		case 6:
 			updateCourseIDUtil();
+			break;
 		case 7:
 			updateCourseInstructorUtil();
-		case 8:
+			break;
+		case 10: {
+
+			dataFile.close();
+			invertedList.close();
+			ofstream temp;
+			temp.open("data.txt", ios::trunc);
+			temp.close();
+			temp.open("inverted.txt", ios::trunc);
+			temp.close();
+			temp.open("primary.txt", ios::trunc);
+			temp.close();
+			temp.open("secondry.txt", ios::trunc);
+			temp.close();
+			atStartUtil();
+			break;
+		}
+		case 11:
+			setStatus("primary.txt", 0);
+			setStatus("secondry.txt", 0);
+			dataFile.close();
+			invertedList.close();
+			atStartUtil();
+			break;
+		case 0:
 			break;
 		default:
 			cout << "Invalid choice\n";
+			break;
 		}
 
 	}
@@ -302,28 +442,29 @@ int maint() {
 
 void atStartUtil()
 {
-	openFile(filedata, "data.txt");
+	openFile(dataFile, "data.txt");
+	openFile(invertedList, "inverted.txt");
 	fetchPrimary();
 	fetchSecondry();
 }
 
 void addCourseUtil()
 {
+	
+
 	Course c;
 	cinCourse(c);
-	filedata.seekp(0, ios::end);
+	if (c.ID[0] == 0) return;
 
-	Primary idx;
-	strcpy(idx.ID, c.ID);
-	idx.offset = filedata.tellp();
+	setStatus("primary.txt", 0);
+	setStatus("secondry.txt", 0);
 
-	primaryIdx.insert(upper_bound(primaryIdx.begin(), primaryIdx.end(), idx), idx);
-
-	writeCourse(c);
+	addCourse(c);
 }
 
 void deleteCourseIDUtil()
 {
+
 	Primary idx;
 	cout << "Enter ID: ";
 	cin.getline(idx.ID, sizeof idx.ID);
@@ -332,17 +473,43 @@ void deleteCourseIDUtil()
 		cout << "ID not found!\n";
 	}
 	else {
-		int offset = primaryIdx[i].offset;
-		primaryIdx.erase(primaryIdx.begin() + i);
 		setStatus("primary.txt", 0);
-		filedata.seekp(offset);
-		filedata.put('*');
+		setStatus("secondry.txt", 0);
+
+		dataFile.seekg(primaryIdx[i].offset);
+		deleteSingleCourse(true);
 	}
 
 }
 
 void deleteCourseInstructorUtil()
 {
+	Secondry idx;
+	cout << "Enter Instructor: ";
+	getline(cin, idx.key);
+	int i = binarySearch(secondryIdx, idx);
+	if (i == -1) {
+		cout << "Instructor not found!\n";
+	}
+	else {
+		setStatus("primary.txt", 0);
+		setStatus("secondry.txt", 0);
+
+		int nxt = secondryIdx[i].labelID;
+		Primary idx2;
+		while (nxt != -1) {
+			invertedList.seekg(nxt * INVERTED_SIZE);
+			invertedList.read((char*)&nxt, sizeof nxt);
+			invertedList.read(idx2.ID, sizeof idx2.ID);
+			int j = binarySearch(primaryIdx, idx2);
+			assert(j != -1);
+
+			Course c;
+			dataFile.seekg(primaryIdx[j].offset);
+			deleteSingleCourse(false);
+		}
+		secondryIdx.erase(secondryIdx.begin() + i);
+	}
 }
 
 void printCourseIDUtil()
@@ -356,7 +523,7 @@ void printCourseIDUtil()
 	}
 	else {
 		int offset = primaryIdx[i].offset;
-		filedata.seekg(offset);
+		dataFile.seekg(offset);
 		Course c;
 		readCourse(c);
 		coutCourse(c);
@@ -373,20 +540,26 @@ void printCourseInstructorUtil()
 		cout << "Instructor not found!\n";
 	}
 	else {
+		int nxt = secondryIdx[i].labelID;
 		Primary idx2;
-		strcpy(idx2.ID, secondryIdx[i].ID);
-		int j = binarySearch(primaryIdx, idx2);
-		assert(j != -1);
-		int offset = primaryIdx[j].offset;
-		filedata.seekg(offset);
-		Course c;
-		readCourse(c);
-		coutCourse(c);
+		while (nxt != -1) {
+			invertedList.seekg(nxt * INVERTED_SIZE);
+			invertedList.read((char*)&nxt, sizeof nxt);
+			invertedList.read(idx2.ID, sizeof idx2.ID);
+			int j = binarySearch(primaryIdx, idx2);
+			assert(j != -1);
+
+			Course c;
+			dataFile.seekg(primaryIdx[j].offset);
+			readCourse(c);
+			coutCourse(c);
+		}
 	}
 }
 
 void updateCourseIDUtil()
 {
+
 	Primary idx;
 	cout << "Enter ID: ";
 	cin.getline(idx.ID, sizeof idx.ID);
@@ -395,33 +568,89 @@ void updateCourseIDUtil()
 		cout << "ID not found!\n";
 	}
 	else {
-		int offset = primaryIdx[i].offset;
+		setStatus("primary.txt", 0);
+		setStatus("secondry.txt", 0);
+		cout << "Enter new course:\n";
 		Course c;
 		cinCourse(c);
-		
-		filedata.seekp(offset);
-		filedata.seekg(offset);
-		int off = updateCourse(c);
-		primaryIdx[i].offset = off;
-		strcpy(primaryIdx[i].ID, c.ID);
-
-		//pretty much just a sort
-		while (i + 1 < (int)primaryIdx.size() && primaryIdx[i + 1] < primaryIdx[i]) {
-			swap(primaryIdx[i + 1], primaryIdx[i]);
-		}
-		while (i - 1 >= 0 && primaryIdx[i] < primaryIdx[i - 1]) {
-			swap(primaryIdx[i - 1], primaryIdx[i]);
-		}
+		dataFile.seekp(primaryIdx[i].offset);
+		updateCourse(c);
 	}
 }
 
 void updateCourseInstructorUtil()
 {
+	Secondry idx;
+	cout << "Enter Instructor: ";
+	getline(cin, idx.key);
+	int i = binarySearch(secondryIdx, idx);
+	if (i == -1) {
+		cout << "Instructor not found!\n";
+	}
+	else {
+		setStatus("primary.txt", 0);
+		setStatus("secondry.txt", 0);
+
+		int nxt = secondryIdx[i].labelID;
+		Primary idx2;
+		assert(nxt != -1);
+		invertedList.seekg(nxt * INVERTED_SIZE);
+		invertedList.read((char*)&nxt, sizeof nxt);
+		invertedList.read(idx2.ID, sizeof idx2.ID);
+		int j = binarySearch(primaryIdx, idx2);
+		assert(j != -1);
+
+		cout << "Enter new course for course " << idx2.ID << " :\n";
+		Course c;
+		cinCourse(c);
+		dataFile.seekg(primaryIdx[j].offset);
+		updateCourse(c);
+	}
+	return;
+
 }
 
 void atEndUtil()
 {
-	filedata.close();
+	dataFile.close();
 	flushPrimary();
 	flushSecondry();
 }
+
+/*
+1
+aaa
+ahmed
+ahmed
+50
+1
+bbb
+nasr
+nasr
+100
+1
+ccc
+dardery
+dardery
+150
+1
+ddd
+ahmed2
+ahmed
+200
+1
+eee
+ahmed3
+ahmed
+250
+1
+fff
+nasr2
+nasr
+300
+1
+ggg
+ibrahim
+ibrahim
+350
+*/
